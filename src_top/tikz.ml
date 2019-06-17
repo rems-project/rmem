33ff00dbc64b5a56998d1e346accb7ba03712b9e
Shaked Flur <fshaked@gmail.com>
2019-06-17 12:57:32 +0100

    wib

--- src_top/tikz.ml
+++ src_top/tikz.ml
@@ -53,7 +53,7 @@ let pp_tex_header out test_info =
   fprintf out "%% Command line: %s\n" (String.concat " " @@ Array.to_list Sys.argv);
   fprintf out "%% Model: %s\n" (Model_aux.pp_model !Globals.model_params);
   fprintf out "%% Litmus hash: %s\n" (try List.assoc "Hash" test_info.Test.info with Not_found -> "???");
-  fprintf out "\n"
+  fprintf out "%%\n"
 
 let make_tikz_graph
     (m:         Globals.ppmode)
@@ -536,9 +536,8 @@ let make_init_state (info: Test.info) (test: Test.test) : unit =
   in
 
   let model_name =
-    let open Params in
     let params = !Globals.model_params in
-    try List.assoc (params.ss.Params.ss_model, params.t.thread_model) Model_aux.model_assoc with
+    try List.assoc (params.ss.ss_model, params.t.thread_model) Model_aux.model_assoc with
     | Not_found -> failwith "Unknown combination of storage and thread sub-systems"
   in
 
@@ -546,16 +545,16 @@ let make_init_state (info: Test.info) (test: Test.test) : unit =
 
   pp_tex_header states_out info;
 
-  fprintf states_out "\\newcommand{\\litmusname}{%s}\n" info.Test.name;
-  fprintf states_out "\\newcommand{\\litmusarch}{%s}\n" (Archs.pp test.Test.arch);
-  fprintf states_out "\\newcommand{\\rmemmodel}{%s}\n" model_name;
-  fprintf states_out "\\newcommand{\\initstate}{%s}\n" init_state;
+  fprintf states_out "\\newcommand{\\litmusname}{%s}%%\n" info.Test.name;
+  fprintf states_out "\\newcommand{\\litmusarch}{%s}%%\n" (Archs.pp test.Test.arch);
+  fprintf states_out "\\newcommand{\\rmemmodel}{%s}%%\n" model_name;
+  fprintf states_out "\\newcommand{\\initstate}{%s}%%\n" init_state;
 
   close_out states_out
 
 let make_final_state (test_info: Test.info) (state: string) : unit =
   let states_out = open_out_gen [Open_creat; Open_append] 0o660 (*rw,rw,r*) ((basename_in_dir test_info.Test.name) ^ ".states.tex") in
-  fprintf states_out "\\newcommand{\\finalstate}{%s}\n" state;
+  fprintf states_out "\\newcommand{\\finalstate}{%s}%%\n" state;
   close_out states_out
 
 module Make (ConcModel: Concurrency_model.S) = struct
